bf08ec50fcc8dd854f818a88043b3e6d8e727282
rohitsamariya01 <rohitsocial2023@gmail.com>
2026-03-19 18:44:00 +0530

    fix: update production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
